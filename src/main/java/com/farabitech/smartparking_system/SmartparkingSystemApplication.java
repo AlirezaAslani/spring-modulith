f0da71761c87a350605bd370bebee895d8b67f3b
AlirezaAslani <a.aslani@mail.irfarabi.com>
2025-11-26 16:18:26 +0330

    fix(SmartparkingSystemApplicatio): use Modulithic instead of Modulith

--- src/main/java/com/farabitech/smartparking_system/SmartparkingSystemApplication.java
+++ src/main/java/com/farabitech/smartparking_system/SmartparkingSystemApplication.java
@@ -2,9 +2,10 @@ package com.farabitech.smartparking_system;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.modulith.Modulithic;
 
 @SpringBootApplication
-//@Modulith(sharedModules = {"common"})
+@Modulithic(sharedModules = {"common"})
 public class SmartparkingSystemApplication {
 
 	public static void main(String[] args) {
